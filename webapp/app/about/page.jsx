32ce92d76b3e02406b2704aef626329797621e74
Julie Moylan <juliemoylan@Julies-MacBook-Air.local>
2026-05-11 23:17:55 +0800

    feat: live Our Story content on the dedicated about page

--- webapp/app/about/page.jsx
+++ webapp/app/about/page.jsx
@@ -1,8 +1,9 @@
+import StorySection from '../../components/StorySection'
+
 export default function About() {
   return (
-    <div className="container" style={{ paddingTop: '100px', paddingBottom: '100px', textAlign: 'center', minHeight: '60vh' }}>
-      <h1 style={{ fontSize: '4rem', color: 'var(--color-purple)', marginBottom: '2rem' }}>About Us</h1>
-      <p style={{ fontSize: '1.4rem' }}>The full story of Lost & Sound coming soon!</p>
-    </div>
+    <main style={{ paddingTop: '60px' }}>
+      <StorySection />
+    </main>
   )
 }
