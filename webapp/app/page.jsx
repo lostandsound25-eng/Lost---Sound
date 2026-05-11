58efe39b14822a77575530648b52e887a937e93d
Julie Moylan <juliemoylan@Julies-MacBook-Air.local>
2026-05-11 23:15:42 +0800

    fix: resolve homepage import conflicts and stabilize story section build

--- webapp/app/page.jsx
+++ webapp/app/page.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import StorySection from '@/components/StorySection'
+import StorySection from '../components/StorySection'
 
 export default function Home() {
   return (
